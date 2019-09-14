83065ad07b0ba4b8de55e2500668fa79d7c0f396
kubo-hide-kun <hide44041@gmail.com>
2019-09-15 00:12:43 +0900

    Update header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,41 @@
 <template>
   <v-app-bar app>
-    <v-toolbar-title class="headline text-uppercase">
-      <span>Vuetify</span>
-      <span class="font-weight-light">MATERIAL DESIGN</span>
+    <v-avatar>
+      <img
+        src="https://pbs.twimg.com/profile_images/1064010537665609728/4oTOHpow_400x400.jpg"
+        alt="icon"
+      >
+    </v-avatar>
+    <v-toolbar-title class="headline">
+      <span>ShoKa Manager</span>
     </v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn
-      text
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-    >
-      <span class="mr-2">Latest Release</span>
+    <v-btn text target="_blank">
+      <span class="mr-2">特徴</span>
     </v-btn>
+    <v-btn text target="_blank">
+      <span class="mr-2">機能</span>
+    </v-btn>
+    <v-btn text target="_blank">
+      <span class="mr-2">ご利用の流れ</span>
+    </v-btn>
+    <v-btn text target="_blank">
+      <span class="mr-2">お問い合わせ</span>
+    </v-btn>
+
+    <v-spacer></v-spacer>
+
+    <v-autocomplete
+      v-model="select"
+      :loading="loading"
+      :items="items"
+      :search-input.sync="search"
+      cache-items
+      class="mx-4"
+      flat
+      hide-no-data
+      hide-details
+      label="ページの検索"
+    ></v-autocomplete>
   </v-app-bar>
 </template>
 
@@ -20,8 +44,50 @@ export default {
   name: 'Header',
   components: {
   },
-  data: () => ({
-    //
-  }),
+  data () {
+    return {
+      loading: false,
+      items: [],
+      search: null,
+      select: null,
+      states: [
+        'ホーム / home',
+        'ログイン / signin',
+        '登録 / signup',
+        '特徴 / feature',
+        '機能 / contents',
+        'ご利用の流れ / usage',
+        'お問い合わせ / contact',
+      ],
+    }
+  },
+  watch: {
+    search (val) {
+      val && val !== this.select && this.querySelections(val)
+    },
+  },
+  mounted() {
+    setInterval(() => {
+      console.log(this.select);
+    }, 500);
+  },
+  methods: {
+    querySelections:  function(v) {
+      this.loading = true
+      // Simulated ajax query
+      setTimeout(() => {
+        this.items = this.states.filter(e => {
+          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
+        })
+        this.loading = false
+      }, 500)
+    },
+  },
 };
 </script>
+
+<style scoped>
+.headline {
+  margin-left: 15px; 
+}
+</style>
