a5d92a1928dac2e28103a38fa78bcd9601dbc5a7
kubo-hide-kun <hide44041@gmail.com>
2019-11-10 14:17:43 +0900

    Make task create form into component

---
+++ src/components/TaskCreateForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-row>
+      <v-dialog v-model="dialog" persistent max-width="60%">
+        <template v-slot:activator="{ on }">
+          <div class="my-4">
+            <v-btn color="success" x-large v-on="on">
+              <b>タスクの新規作成</b>
+            </v-btn>
+          </div>
+        </template>
+        <v-card>
+          <v-card-title class="headline">Use Google's location service?</v-card-title>
+          <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+            <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
--- src/views/GanttChart.vue
+++ src/views/GanttChart.vue
@@ -2,27 +2,7 @@
   <div class="demo-app">
     <div>
       <div class="demo-app-top">
-        <v-row>
-          <v-dialog v-model="dialog" persistent max-width="60%">
-            <template v-slot:activator="{ on }">
-              <div class="my-4">
-                <v-btn color="success" x-large v-on="on">
-                  <b>タスクの新規作成</b>
-                </v-btn>
-              </div>
-            </template>
-            <v-card>
-              <v-card-title class="headline">Use Google's location service?</v-card-title>
-              <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-                <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-row>
-
+        <TaskCreateForm />
         <v-bottom-navigation :value="isGanttChart" color="deep-purple accent-4">
           <v-btn @click="isGanttChart = 0">
             <span>Calendar</span>
@@ -90,13 +70,15 @@ import "@fullcalendar/timegrid/main.css";
 import "@fullcalendar/timeline/main.css";
 import "@fullcalendar/resource-timeline/main.css";
 
+import TaskCreateForm from "../components/TaskCreateForm";
+
 export default {
   components: {
-    FullCalendar // make the <FullCalendar> tag available
+    FullCalendar, // make the <FullCalendar> tag available
+    TaskCreateForm
   },
   data: function() {
     return {
-      dialog: false,
       isGanttChart: 0,
       isMobileView: false,
       calendarPlugins: [
