96afbaae0dbbecc08940552e1d786520c9615ed9
kubo-hide-kun <hide44041@gmail.com>
2019-11-10 09:35:19 +0900

    Add support for project list by component

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-card dark tile flat color="grey darken-2" class="project-select">
+    <v-card-text>
+      ＃プロジェクト名
+      <br />＃プロジェクト名
+    </v-card-text>
+  </v-card>
+</template>
