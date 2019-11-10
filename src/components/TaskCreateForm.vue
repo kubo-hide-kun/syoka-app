1b773d23d21ae706019d15e828f981e2626f81cf
kubo-hide-kun <hide44041@gmail.com>
2019-11-10 15:07:24 +0900

    Add support for markdown editor on task create modal

--- src/components/TaskCreateForm.vue
+++ src/components/TaskCreateForm.vue
@@ -10,13 +10,19 @@
           </div>
         </template>
         <v-card>
-          <v-card-title class="headline">Use Google's location service?</v-card-title>
-          <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-            <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-          </v-card-actions>
+          <div class="ma-6">
+            <v-card-title class="headline">新規作成</v-card-title>
+            <v-text-field v-model="title" label="イベント名" outlined></v-text-field>
+            <p>・詳細(MarkDown対応)</p>
+            <v-card elevation=2>
+              <mavon-editor v-model="description" language="ja" ref="md" />
+            </v-card>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+              <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+            </v-card-actions>
+          </div>
         </v-card>
       </v-dialog>
     </v-row>
@@ -24,10 +30,18 @@
 </template>
 
 <script>
+import Vue from "vue";
+import mavonEditor from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+Vue.use(mavonEditor);
+
 export default {
   data() {
     return {
-      dialog: false
+      dialog: false,
+
+      title: "",
+      description: ""
     };
   }
 };
