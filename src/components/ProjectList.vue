eb86fc0562562562b88fd6bc89bfff95cb67cf7c
kubo-hide-kun <hide44041@gmail.com>
2019-12-18 14:14:58 +0900

    Create form to create new project

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -15,14 +15,29 @@
               </div>
             </template>
             <v-card>
-              <div class="ma-6">
+              <div class="ma-6" v-if="modeFrag">
                 <v-card-title class="headline">プロジェクトを追加する</v-card-title>
                 <v-text-field v-model="title" label="参加したプロジェクト名を入力" outlined></v-text-field>
-                <v-btn text small color="primary">プロジェクトを新規作成する</v-btn>
+                <v-btn text small color="primary" @click="modeFrag = false">プロジェクトを新規作成する</v-btn>
                 <v-card-actions>
                   <v-spacer />
-                  <v-btn color="green darken-1" text @click="dialog = false">キャンサル</v-btn>
+                  <v-btn color="green darken-1" text @click="dialog = false">キャンセル</v-btn>
                   <v-btn color="green darken-1" text @click="post">参加</v-btn>
+                </v-card-actions>
+              </div>
+              <div class="ma-6" v-else>
+                <v-card-title class="headline">プロジェクトの新規作成</v-card-title>
+                <v-text-field v-model="title" label="プロジェクト名" outlined></v-text-field>
+                <p>・詳細(MarkDown対応)</p>
+                <v-card elevation="2">
+                  <mavon-editor v-model="description" language="ja" ref="md" />
+                </v-card>
+                <v-spacer />
+                <v-btn text small color="primary" @click="modeFrag = true" class="ma-2">既存プロジェクトに参加する</v-btn>
+                <v-card-actions>
+                  <v-spacer />
+                  <v-btn color="green darken-1" text @click="dialog = false">キャンセル</v-btn>
+                  <v-btn color="green darken-1" text @click="post">新規作成</v-btn>
                 </v-card-actions>
               </div>
             </v-card>
@@ -53,6 +68,7 @@ export default {
   data() {
     return {
       dialog: false,
+      modeFrag: true,
       projects: [
         { title: "A-projects", isFire: true },
         { title: "B-projects", isFire: false },
