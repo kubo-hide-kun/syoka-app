843ed91ad19d47078d4a07814edfe443a33528cd
nekozuki0114 <nekozuki.090@gmail.com>
2019-12-20 16:04:31 +0900

    Revert "login"
    This reverts commit 5bdda3f

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDRgYDofdrqDKVROMW1cXsEht41TCfdQjQ",
+    authDomain: "fireworks-b4b26.firebaseapp.com",
+    databaseURL: "https://fireworks-b4b26.firebaseio.com",
+    projectId: "fireworks-b4b26",
+    storageBucket: "fireworks-b4b26.appspot.com",
+    messagingSenderId: "858666908632",
+    appId: "1:858666908632:web:7297a3dc681a59e2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebaseApp.firestore();
+firestore.settings({ timestampsInSnapshots: true });
+
 
 new Vue({
   router,
